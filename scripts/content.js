e79f0d39d4efff6d8434595f523a067d164acfb8
Oliver Crossley <ollythecrossley@outlook.com>
2024-04-01 18:23:45 +0100

    removes funny console log

--- scripts/content.js
+++ scripts/content.js
@@ -1,7 +1,5 @@
 let button = document.getElementById("ot-sdk-btn-floating")
 
-console.log("Did this work...?")
-
 if (button) {
     button.parentNode.removeChild(button)
     console.log("HIDIVE Cookie Remover:\tRemoved successfully!")
