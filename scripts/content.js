2f3c277942f2ec1c3165ab320cc976d5d5371576
Oliver Crossley <ollythecrossley@outlook.com>
2024-04-01 18:36:40 +0100

    revamps code with mutation watcher instead of checking webpage

--- scripts/content.js
+++ scripts/content.js
@@ -1,8 +1,18 @@
-let button = document.getElementById("ot-sdk-btn-floating")
-
-if (button) {
+function handleCookieButton(button) {
     button.parentNode.removeChild(button)
     console.log("HIDIVE Cookie Remover:\tRemoved successfully!")
-} else {
-    console.warn("HIDIVE Cookie Remover:\tCould not find Cookie Button...")
 }
+
+const observer = new MutationObserver(function (mutations, mutationInstance) {
+    let button = document.getElementById("ot-sdk-btn-floating")
+    if (button) {
+        handleCookieButton(button);
+        mutationInstance.disconnect();
+    }
+});
+
+
+observer.observe(document, {
+    childList: true,
+    subtree:   true
+});
